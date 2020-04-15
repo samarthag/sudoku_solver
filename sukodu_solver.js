7f72e34a1fe6ff35ec8d94eaa276ac270af12123
samarthag <62885755+samarthag@users.noreply.github.com>
2020-04-15 12:46:13 +0530

    Add files via upload

---
+++ sukodu_solver.js
@@ -0,0 +1,48 @@
+const _board = [
+    ['0', '0', '0', '0', '0', '0', '6', '0', '9'],
+    ['0', '0', '9', '0', '7', '0', '0', '5', '0'],
+    ['0', '0', '4', '9', '3', '0', '0', '1', '0'],
+    ['3', '0', '2', '0', '0', '0', '0', '7', '0'],
+    ['0', '0', '7', '2', '8', '3', '9', '0', '0'],
+    ['0', '5', '0', '0', '0', '0', '1', '0', '3'],
+    ['0', '3', '0', '0', '1', '9', '2', '0', '0'],
+    ['0', '2', '0', '0', '5', '0', '7', '0', '0'],
+    ['7', '0', '6', '0', '0', '0', '0', '0', '0'],
+];
+sodokoSolver(_board);
+console.log(_board);
+function isValid(board, row, col, k) {
+    for (let i = 0; i < 9; i++) {
+        const m = 3 * Math.floor(row / 3) + Math.floor(i / 3);
+        const n = 3 * Math.floor(col / 3) + i % 3;
+        console.log('Row x i :' + board[row][i]);
+        console.log('i x col :' + board[i][col]);
+        console.log('m x n :' + board[m][n]);
+        if (board[row][i] == k || board[i][col] == k || board[m][n] == k) {
+          return false;
+        }
+    }
+    return true;
+}
+function sodokoSolver(data) {
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      console.log('Data at i x j:' + data[i][j])
+      if (data[i][j] == '0') {
+        for (let k = 1; k <= 9; k++) {
+          if (isValid(data, i, j, k)) {
+            data[i][j] = `${k}`;
+            console.log('Updated value Data at i x j:' + data[i][j])
+          if (sodokoSolver(data)) {
+           return true;
+          } else {
+           data[i][j] = '0';
+          }
+         }
+       }
+       return false;
+     }
+   }
+ }
+ return true;
+}
